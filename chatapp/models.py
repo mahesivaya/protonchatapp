b81097d7515a34fb46d86acd924c8e0f3bbf511d
mahesivaya <167259736+mahesivaya@users.noreply.github.com>
2025-11-30 11:14:46 -0500

    save messages into database

--- chatapp/models.py
+++ chatapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
+class SelfMessage(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(default=timezone.now)
